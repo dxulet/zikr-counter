24d65fdaaaf51bd0efee550937eb6e99f9ad5c89
Daulet Ashikbayev <dxulet@gmail.com>
2023-01-06 20:27:34 +0500

    Initial Commit

---
+++ ZikrCounter/ZikrCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZikrCounterApp.swift
+//  ZikrCounter
+//
+//  Created by Daulet Ashikbayev on 06.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct ZikrCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
